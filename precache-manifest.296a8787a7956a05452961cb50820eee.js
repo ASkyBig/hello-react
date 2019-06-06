288e3d19e167406bb795249afc0c9753a447d815
p_jianjyang <p_jianjyang@tencent.com>
2019-06-06 20:49:55 +0800

    Updates

---
+++ precache-manifest.296a8787a7956a05452961cb50820eee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d59e805d2002f13d10c319eb267b941",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/hello-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e8143390e33375070faa",
+    "url": "/hello-react/static/js/main.3efd3825.chunk.js"
+  },
+  {
+    "revision": "69dcf12e7815e7046f81",
+    "url": "/hello-react/static/js/runtime~main.d4bee3ea.js"
+  }
+]);
